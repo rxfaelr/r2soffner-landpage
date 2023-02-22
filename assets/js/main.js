418b421c25e577fed7d34ccf6daa16a0f35a6800
rxfaelr <rafa331rodri@gmail.com>
2023-02-22 13:16:12 -0300

    mobile navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -86,12 +86,25 @@
     onscroll(document, toggleBacktotop)
   }
 
-  /*navbar mobile*/
+  /**
+   * Mobile nav toggle
+   */
   on('click', '.mobile-nav-toggle', function(e) {
     select('#navbar').classList.toggle('navbar-mobile')
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
   })
+
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on('click', '.navbar .dropdown > a', function(e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+      e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
+    }
+  }, true)
+
 
   /*animacao*/
   function aos_init() {
